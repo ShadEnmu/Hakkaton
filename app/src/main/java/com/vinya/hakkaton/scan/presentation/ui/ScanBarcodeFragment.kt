7531fdf6085f4667855e733bb01fd56dd957b11d
ShadEnmu <111167441+ShadEnmu@users.noreply.github.com>
2022-11-23 17:51:35 +0800

    master

--- app/src/main/java/com/vinya/hakkaton/scan/presentation/ui/ScanBarcodeFragment.kt
+++ app/src/main/java/com/vinya/hakkaton/scan/presentation/ui/ScanBarcodeFragment.kt
@@ -1,10 +1,16 @@
 package com.vinya.hakkaton.scan.presentation.ui
 
+import android.os.Bundle
+import android.view.View
 import com.vinya.hakkaton.R
 import com.vinya.hakkaton.core.presentation.BaseFragment
 
 class ScanBarcodeFragment : BaseFragment() {
     override val layoutId: Int = R.layout.fragment_barcode
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
     companion object{
         @JvmStatic
         fun newInstance(): ScanBarcodeFragment {
