51422a06e7f77bbdfe526adcf2c2ec691d220c20
Xikka03 <cvadim627@gmail.com>
2022-11-23 15:09:15 +0800

    master

--- app/src/main/java/com/vinya/hakkaton/MainActivity.kt
+++ app/src/main/java/com/vinya/hakkaton/MainActivity.kt
@@ -10,6 +10,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val textView : TextView = findViewById(R.id.textview)
         textView.text = hello
-       // VADIM LOHasd asd8564652198
+       // VADIM NE LOH
     }
 }
