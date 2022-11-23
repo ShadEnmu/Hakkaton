c4ba9b6b09e30063356dc927e23aadcb8fb944bd
Xikka03 <cvadim627@gmail.com>
2022-11-23 14:52:30 +0800

    3

--- app/src/main/java/com/vinya/hakkaton/MainActivity.kt
+++ app/src/main/java/com/vinya/hakkaton/MainActivity.kt
@@ -7,5 +7,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }//56465496654
+    }//56145623156
 }
